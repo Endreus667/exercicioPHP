beb91a145ea58e18029d5a7d6f651ac367e8825d
Endreus667 <endreus.kenny.figueiro@gmail.com>
2024-10-03 16:54:37 -0300

    criaçao

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+
+$lista = [
+    'nome' => 'end',
+    'idade' => 19,
+    'atributos' => ['força' => 100, 'agilidade' => 80, 'destreza' => 70],
+    $vida = 1000,
+    $mana = 900,
+
+];
+
+echo "NOME: ".$lista['nome']."<br/>";
+echo "FORÇA: ".$lista['atributos']['agilidade']."<br/>";
+echo "VIDA: ".$vida;
